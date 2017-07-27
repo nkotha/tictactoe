5f359aa52b5b4ded6d915ff5a5322c119d44fd64
Nandita Kotha <nanditakotha@tutor-group.com>
2017-07-27 16:57:00 -0700

    ES6 code for contains

--- script.js
+++ script.js
@@ -17,3 +17,10 @@ function startNewGame() {
     square.innerHTML = EMPTY;
   });
 }
+
+function contains(selector, text) {
+  var elements = document.querySelectorAll(selector);
+  return [].filter.call(elements, function(element){
+    return RegExp(text).test(element.textContent);
+  });
+}
