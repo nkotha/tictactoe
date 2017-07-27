b60b7fd5d94235e5b02af30b9c932dda63e2741f
Nandita Kotha <nanditakotha@tutor-group.com>
2017-07-27 16:56:17 -0700

    Code for starting the game

--- script.js
+++ script.js
@@ -4,3 +4,16 @@ var N_SIZE = 3,
     turn = "X",
     score,
     moves;
+
+
+function startNewGame() {
+  score = {
+    "X": 0,
+    "O": 0
+  };
+  moves = 0;
+  turn = "X";
+  boxes.forEach(function (square) {
+    square.innerHTML = EMPTY;
+  });
+}
